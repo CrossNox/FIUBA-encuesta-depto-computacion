0fa8348399b5b639b53818846afb789bb68e25f0
CrossNox <ijmermet@gmail.com>
2021-04-09 05:22:58 -0300

    update and fixes

--- utils/compile_to_pdf.py
+++ utils/compile_to_pdf.py
@@ -1,22 +1,26 @@
 """Export notebook to pdf using custom template."""
 
-from nbconvert import PDFExporter
 import argparse
+from pathlib import Path
 
-if __name__ == '__main__':
+from nbconvert import PDFExporter
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    parser.add_argument('input', type=str, help='Input file')
-    parser.add_argument('--output', '-o', type=str, help='Output file')
+    parser.add_argument("input", type=str, help="Input file")
+    parser.add_argument("--output", "-o", type=str, help="Output file")
 
     args = parser.parse_args()
 
-    pdfexp = PDFExporter(template_file="hidecode.tpl")
+    pdfexp = PDFExporter(
+        template_file=str((Path(__file__).parent / "hidecode.tpl").resolve())
+    )
 
     pdf_data, _ = pdfexp.from_filename(args.input)
 
-    output_file = args.output_file or f"{args.input}.rstrip('.ipynb').pdf"
+    output_file = args.output or f"{args.input.rstrip('.ipynb')}.pdf"
 
     with open(output_file, "wb") as f:
         f.write(pdf_data)
